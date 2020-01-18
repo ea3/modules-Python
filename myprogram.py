6262c2db393c8cc9d2873e1a3801e2ef4e548641
Emilio Araos <ea3dev@gmail.com>
2020-01-18 00:41:39 -0700

    created packages and modules

--- myprogram.py
+++ myprogram.py
@@ -1,3 +1,6 @@
-from mymodule import myfunc
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
 
-myfunc()
+
+some_main_script.report_main()
+mysubscript.sub_report()
